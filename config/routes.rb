22c4997dcf3476a574f4bb25900aa3544632235b
Cameron Romo <cromo1225@gmail.com>
2022-12-15 18:01:59 -0700

    Add task mvc initial set-up

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
+  get 'tasks/index'
+  get 'tasks/new'
   get 'welcome/index'
   root 'welcome#index'
+  resources :tasks
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
