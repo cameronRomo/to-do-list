12de4b18c9d303908e2e3185753b07683d031992
Cameron Romo <cromo1225@gmail.com>
2022-12-19 16:22:21 -0700

    Seed data into db

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,22 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+Task.create([
+  {
+    title: "Drop it Like it's Hot",
+    details: "Drop it!"
+  },
+  {
+    title: "Go To the Store",
+    details: "Need pasta"
+  },
+  {
+    title: "Eat a Good Breakfast",
+    details: "Cake, Coffee, etc."
+  },
+  {
+    title: "Make a Rails App",
+    details: "AirBnB, but with tents?"
+  }
+])
