f1457119cce90c3cd4d192e0849226d986777e81
Cameron Romo <cromo1225@gmail.com>
2022-12-19 12:16:26 -0700

    Add destroy and show actions

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  get 'tasks/index'
-  get 'tasks/new'
-  get 'welcome/index'
+  # get 'tasks/index'
+  # get 'tasks/new'
+  # get 'welcome/index'
   root 'welcome#index'
   resources :tasks
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
