6223c8a8cf1f26aa1a8c67825bcd035018705fa5
Cameron Romo <cromo1225@gmail.com>
2022-12-19 14:59:24 -0700

    Add migration to add completed column for a task

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_12_16_004420) do
+ActiveRecord::Schema[7.0].define(version: 2022_12_19_215400) do
   create_table "tasks", force: :cascade do |t|
     t.string "title"
     t.text "details"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "completed", default: false
   end
 
 end
