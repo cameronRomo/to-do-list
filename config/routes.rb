81eb4baf3104c58d7d6d86d1affce0cfd35abc7b
Cameron Romo <cromo1225@gmail.com>
2022-12-21 11:13:53 -0700

    Add session object

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,17 @@
 Rails.application.routes.draw do
-  resources :users
   get 'welcome/index'
   root 'welcome#index'
+  
+  resources :sessions, only: [:new, :create, :destroy]
+  get 'log_in', to: 'sessions#new'
+  get 'log_out', to: 'sessions#destroy'
+
+  resources :users
+  get 'sign_up', to: 'users#new'
+
   resources :tasks
 
   get 'tasks/:id/complete/:completed', to: "tasks#complete"
-
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
