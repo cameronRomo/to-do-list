c25965d85fff60cb72fd59519dd8649895740f29
Cameron Romo <cromo1225@gmail.com>
2022-12-20 18:32:09 -0700

    Seed user into db

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,8 @@
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
 
+User.create(username: "Cam", password: "password")
+
 Task.create([
   {
     title: "Drop it Like it's Hot",
