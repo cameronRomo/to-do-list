5631a11ae1d474bea657e4e3f96a9b3916705426
Cameron Romo <cromo1225@gmail.com>
2022-12-20 18:20:45 -0700

    Add initial user scaffold

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_12_19_215400) do
+ActiveRecord::Schema[7.0].define(version: 2022_12_21_011840) do
   create_table "tasks", force: :cascade do |t|
     t.string "title"
     t.text "details"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.boolean "completed", default: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.string "password_digest"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
